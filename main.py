55d45f6535ee7ff1493e47046abcd0fa1ad1ad65
baymax55 <xingbai56@gmail.com>
2023-04-26 17:55:29 +0800

    return expiration date

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ from decimal import Decimal, ROUND_HALF_UP
 usage_url = '/v1/dashboard/billing/usage'
 # 查询订阅信息URL
 subscription_url = '/v1/dashboard/billing/subscription'
-host = 'https://api.openai.com'
 
-apikey = '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'
+expire_time_url = 'dashboard/billing/credit_grants'
+host = 'https://api.openai.com'
+apikey = 'sk-xxx'
 # set params as a dictionary
 params = {
     # start date is 90 days ago
@@ -26,6 +27,7 @@ headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + apik
 proxies = {'http': 'http://127.0.0.1:7890', 'https': 'https://127.0.0.1:7890'}
 
 # send POST request
+# expire_response = requests.get(host + expire_time_url, headers=headers)
 usage_response = requests.get(host + usage_url, headers=headers, params=params)
 subscription_response = requests.get(host + subscription_url, headers=headers)
 
@@ -35,6 +37,9 @@ subscription_result = subscription_response.text
 
 # 获取总额
 total_usd = Decimal(json.loads(subscription_result)['hard_limit_usd'])
+expire_seconds = Decimal(json.loads(subscription_result)['access_until'])
+date = datetime.fromtimestamp(int(expire_seconds))
+
 # 获取使用量
 total_usage = Decimal(json.loads(usage_result)['total_usage'])
 
@@ -43,3 +48,4 @@ usage_in_dollars = total_usage / Decimal('100').quantize(Decimal('.001'), roundi
 
 print(f"实际已使用金额:{usage_in_dollars}")
 print(f"总共金额:{total_usd}")
+print(f"过期日期:{date.isoformat()}")
