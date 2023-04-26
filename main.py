34a5ccb9093f6d7f572a01a258bb454372114a79
baymax55 <xingbai56@gmail.com>
2023-04-26 15:44:51 +0800

    init

---
+++ main.py
@@ -0,0 +1,45 @@
+import requests
+from datetime import datetime, timedelta
+import json
+from decimal import Decimal, ROUND_HALF_UP
+
+# set url and form data
+# 查询使用量URL
+usage_url = '/v1/dashboard/billing/usage'
+# 查询订阅信息URL
+subscription_url = '/v1/dashboard/billing/subscription'
+host = 'https://api.openai.com'
+
+apikey = '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'
+# set params as a dictionary
+params = {
+    # start date is 90 days ago
+    'start_date': (datetime.now() - timedelta(days=90)).strftime('%Y-%m-%d'),
+    # end date is tomorrow
+    'end_date': (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d')
+}
+
+# set headers and authorization
+headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + apikey}
+
+# set proxies
+proxies = {'http': 'http://127.0.0.1:7890', 'https': 'https://127.0.0.1:7890'}
+
+# send POST request
+usage_response = requests.get(host + usage_url, headers=headers, params=params)
+subscription_response = requests.get(host + subscription_url, headers=headers)
+
+# get response body
+usage_result = usage_response.text
+subscription_result = subscription_response.text
+
+# 获取总额
+total_usd = Decimal(json.loads(subscription_result)['hard_limit_usd'])
+# 获取使用量
+total_usage = Decimal(json.loads(usage_result)['total_usage'])
+
+# divide by 100 to convert from cents to dollars
+usage_in_dollars = total_usage / Decimal('100').quantize(Decimal('.001'), rounding=ROUND_HALF_UP)
+
+print(f"实际已使用金额:{usage_in_dollars}")
+print(f"总共金额:{total_usd}")
